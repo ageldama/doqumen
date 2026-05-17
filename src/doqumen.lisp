9e207d8033034bc9e0e5d6149d73f9dc545c81d2
jonghyouk yun <ageldama@gmail.com>
2026-05-17 20:47:57 +0900

    [WIP]

--- src/doqumen.lisp
+++ src/doqumen.lisp
@@ -287,11 +287,12 @@
 
 
 
+(defvar *anchor-uri-encode-func*  #'quri:url-encode)
 
 
 (defun print-html-anchor (text anchor-uri)
   (format *out-stream* "<a name=\"~a\">~a</a>~%"
-          (quri:url-encode anchor-uri)
+          (funcall *anchor-uri-encode-func* anchor-uri)
           (cl-who:escape-string text)))
 
 
@@ -311,18 +312,42 @@
   )
 
 
+
+(defvar *print-toc-title* "# Table of Contents")
+
+
+(defun print-toc-as-markdown
+    (toc &key (out-stream *out-stream*) (level 1))
+  (when (eq 1 level)
+    (format out-stream "~A~%~%" *print-toc-title*))
+  (dolist (e toc)
+    ;; TODO skip keyword?
+    (format out-stream "~v@{~A~:*~}" level "  ")
+    (format out-stream "1. [~A](~A)~%"
+            (getf e :text)
+            (funcall *anchor-uri-encode-func*
+                     (format nil "~A" (getf e :anchor))))
+    (when (getf e :children)
+      (print-toc-as-markdown (getf e :children)
+                             :out-stream out-stream
+                             :level (1+ level))))
+  (when (eq 1 level)
+    (format out-stream "~%")))
+
+
+
+(defvar *print-toc-func* #'print-toc-as-markdown)
+
+
 (defun print-toc ()
-  ;; TODO
   (log:info "PRINT TOC ...")
-  (print *toc* *out-stream*)
-  )
+  (funcall *print-toc-func* *toc*))
 
 
 
 
-(defmacro with-sections ((&key (v-section 'section)) &rest body)
-  `(dolist (,v-section (getf *seed-plist* :sections))
-     ,@body))
+(defun root-sections ()
+    (getf *seed-plist* :sections))
 
 
 (defun find+apply (prefix sym &rest args)
@@ -334,58 +359,63 @@
          args))
 
 
-;; TODO child-sections / recur
 
-(defmacro toc-appendf (place &key text anchor)
-  `(progn (appendf ,place (list (list :text ,text
-                               :anchor ,anchor)))
-          ,place))
-
-
-(defun build-toc ()
-  (let ((toc nil))
-    (with-sections (:v-section section)
-                   (if (keywordp section)
-                       t
-                       ;; else:
-                       (toc-appendf toc :text section :anchor section))
-    toc)))
+(defmacro toc-appendf (place &key text anchor children)
+  (let ((%lst (gensym)))
+    `(let ((,%lst '()))
+       (appendf ,%lst (list :text ,text :anchor ,anchor))
+       (when ,children
+         (appendf ,%lst (list :children ,children)))
+       (appendf ,place (list ,%lst))
+       ;;
+       ,place)))
 
 
+(defun build-toc
+    (&key sections)
+  (let ((toc nil)
+        (sections* (or sections (root-sections))))
+    (dolist (section sections*)
+      (cond
+        ((listp section)
+         (let* ((subl-l (build-toc :sections (list (first section))))
+                (subl-r (build-toc :sections (rest section))))
+           (toc-appendf toc
+                        :text (getf (first subl-l) :text)
+                        :anchor (getf (first subl-l) :anchor)
+                        :children subl-r)))
+        ((keywordp section)
+         (toc-appendf toc :text section :anchor section))
+        (t
+         (toc-appendf toc :text section :anchor section))))
+    toc))
 
-;;; section.1
-;;; section.2
-;;; ...
-;;; api-ref / pkg-1
-;;; api-ref / pkg-1 / symbol-1 = type-1
-;;; api-ref / pkg-1 / symbol-1 = type-2
 
 
-(defun print-sections ()
-  ;; (format *out-stream* "~a ~A~%" *seed-plist* *output-pn*)
-  (with-sections (:v-section section)
-                 (if (keywordp section)
-                     (case section
-                       (:toc (print-toc))
-                       (:api-ref (print-api-ref))
-                       ;; They told me I could be anything so I...:
-                       (t (find+apply "print-" section)))
-                     ;; else: just a section
-                     (let ((in-pn
-                             (merge-pn-with-asdf-system-path section *system-name*)))
-                       (log:debug "COPYING FROM: ~a" in-pn)
-                       (copy-file-into-stream in-pn *out-stream*)))))
+
+
+(defun print-sections
+    (&key sections)
+  (let ((sections* (or sections (root-sections))))
+    (dolist (section sections*)
+      (cond
+        ((keywordp section)
+          (case section
+            (:toc (print-toc))
+            (:api-ref (print-api-ref))
+            ;; They told me I could be anything so I...:
+            (t (find+apply "print-" section))))
+        ((listp section)
+         (print-sections :sections section))
+        (t ;; else: just a section
+          (let ((in-pn
+                  (merge-pn-with-asdf-system-path section *system-name*)))
+            (log:debug "COPYING FROM: ~a" in-pn)
+            (copy-file-into-stream in-pn *out-stream*)))))))
 
 
 (defun init-logger ()
-  (log:config
-   :debug
-   ;; :notime
-   ;; :nofile
-   ;; :nopackage
-   ;; :nopretty
-   ;; :1line
-   ))
+  (log:config :debug))
 
 
 
